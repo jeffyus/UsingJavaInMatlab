e12029189ce31e6bd36f84e8cfc624c8f81339d5
Jianfei Yu <39782394+jeffyus@users.noreply.github.com>
2023-02-15 17:30:32 +0000

    replaceZipEntry.m

---
+++ nio/N02_ReplaceZipEntry/replaceZipEntry.m
@@ -0,0 +1,15 @@
+function replaceZipEntry(zipFileName, entryName)
+% Quickly replace existing entry Using java.nio.file.FileSystems
+
+entryFilePath = java.nio.file.Paths.get(entryName, javaArray('java.lang.String', 0));
+zipFilePath = java.nio.file.Paths.get(zipFileName, javaArray('java.lang.String', 0));
+
+zipFileSystem = javaMethod('newFileSystem', 'java.nio.file.FileSystems', zipFilePath, []);
+zipEntryPath = zipFileSystem.getPath(['/' entryName], javaArray('java.lang.String', 0));
+
+copyOption = javaArray('java.nio.file.StandardCopyOption',1);
+copyOption(1) = java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+javaMethod('copy', 'java.nio.file.Files', entryFilePath, zipEntryPath, copyOption)
+zipFileSystem.close
+
+end
